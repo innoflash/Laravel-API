0b3b4bbd0267c23b1a51038496bdbdeeb2f639e0
innoflash <innocentmazando@gmail.com>
2021-06-22 18:17:04 +0200

    added logo

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -7,7 +7,7 @@
 <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
     <div class="max-w-md w-full space-y-8">
         <div>
-            <img class="mx-auto h-56 w-auto" src="{{ asset('logo.png') }}"
+            <img class="mx-auto h-56 w-auto" src="{{ asset('images/logo.png') }}"
                  alt="Workflow">
             <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
                 Reset Your Password
